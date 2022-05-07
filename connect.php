3ec20e88c9b74aab67f1796a7c6b04abfa96e0ba
Ammar Ihsan Nasution <ammarihsan04@gmail.com>
2022-05-07 16:21:12 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,12 +2,12 @@
 error_reporting(E_ALL ^ E_DEPRECATED | E_NOTICE | E_WARNING);
 error_reporting( error_reporting() & ~E_NOTICE );
 
-$link = mysql_connect( 'localhost', 'puanmary_cubic', 'adminCUBIC1' );
+$link = mysql_connect( 'localhost', 'dbuser', 'password' );
 if ( !$link ) {
   die( 'Could not connect: ' . mysql_error() );
 }
 
-$db = mysql_select_db( 'puanmary_cubic', $link );
+$db = mysql_select_db( 'dbname', $link );
 if ( !$db ) {
   die ( 'Error selecting database \'puanmary_cubic\' : ' . mysql_error() );
 }
